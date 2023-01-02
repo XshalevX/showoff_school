a2221fe0646f0791903f6f84e5b8d4587a964f97
Shalev Belaish <xshalevx1@gmail.com>
2023-01-03 00:41:02 +0200

    finished project

---
+++ src/Hack.js
@@ -0,0 +1,21 @@
+import './Hacked.css'
+export default function Hack()
+{
+
+    return(
+    <div
+      style={{ backgroundImage: "url(" + "https://images5.alphacoders.com/423/423529.jpg" + ")", height:700,backgroundPosition: 'center',
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat',
+      height: 1000,
+        display: 'flex',
+        justifyContent:'center',
+        alignItems:'center'
+        
+    
+    }}
+    >
+      <h1 style={{fontSize:80, color:'white',textAlign: 'center', fontFamily:"HACKED"}} >YOU HAVE BEEN HACKED</h1>
+      </div>
+    )
+}
